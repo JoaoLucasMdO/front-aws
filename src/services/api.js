d821bd6cfb0b0c32ed904d352abb2f1d18d480b7
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2025-06-14 11:29:18 -0300

    fix - link api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://dsm-vot-api-p2.duckdns.org/",
+  baseURL: "http://18.215.24.30/",
   headers: {
     "Content-Type": "application/json",
   },
